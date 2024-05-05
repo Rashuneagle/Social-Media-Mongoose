23e383a9ac5994c26ef591661eeff0974d63e8b8
Michael Scott <mscotteagle21@gmail.com>
2024-05-04 20:42:55 -0500

    created Express app, started on the models, imported libraries

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^[\w-]+(?:\.[\w-]+)*@(?:[\w-]+\.)+[a-zA-Z]{2,7}$/,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+applicationSchema
+  .virtual('friendCount')
+  // Getter
+  .get(function () {
+    return this.friends.length;
+  });
+
+// Create and export the User model
+module.exports = model('User', userSchema);
